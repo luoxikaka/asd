bb1c527d04b2bb6ce3187d051ad2b05cd960df93
luoxikaka <luoxikaka@users.noreply.github.com>
2026-03-03 16:46:09 +0800

    just test

--- baidu_openclaw_news_playwright.py
+++ baidu_openclaw_news_playwright.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 
-
+123
 def fetch_openclaw_news(limit: int = 2) -> list[dict[str, str]]:
     """Search `openclaw` on Baidu News and return top news results."""
     with sync_playwright() as p:
